49e2e09087a4b2476b4af71acca3a20e312a1652
mmmint <mmmintstudio@gmail.com>
2023-08-02 23:41:46 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/todo123/', // add this property and the value is the name of your repository
 	test: {
 		globals: true,
 		environment: "jsdom",
